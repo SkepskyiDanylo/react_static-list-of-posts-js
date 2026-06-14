0d9cc3e663df9a657be1ad66b36bf0bf7153c311
Danylo Skepskyi <skepskyidanylo@gmail.com>
2026-06-14 14:13:23 +0200

    fix

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,10 +1,13 @@
+import React from 'react';
 import { CommentInfo } from '../CommentInfo';
 import './CommentList.scss';
 
 export const CommentList = ({ comments }) => (
   <div className="CommentList">
     {comments.map(comment => (
-      <CommentInfo key={comment.id} comment={comment} />
+      <React.Fragment key={comment.id}>
+        <CommentInfo comment={comment} />
+      </React.Fragment>
     ))}
   </div>
 );
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,9 +1,12 @@
+import React from 'react';
 import { PostInfo } from '../PostInfo';
 
 export const PostList = ({ posts }) => (
   <div className="PostList">
     {posts.map(post => (
-      <PostInfo key={post.id} post={post} />
+      <React.Fragment key={post.id}>
+        <PostInfo post={post} />
+      </React.Fragment>
     ))}
   </div>
 );
